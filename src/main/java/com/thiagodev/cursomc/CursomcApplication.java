474b7f63c6254c7b74e8e43a2e46fab40cfd6f9a
lenx0 <thiagofreitas1106@gmail.com>
2021-06-19 12:35:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/thiagodev/cursomc/CursomcApplication.java
+++ src/main/java/com/thiagodev/cursomc/CursomcApplication.java
@@ -1,13 +1,41 @@
 package com.thiagodev.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.thiagodev.cursomc.domain.Categoria;
+import com.thiagodev.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//salva os 2 objetos dentro de uma lista e vai para o banco
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
 }
+
+
+//A implementação CommandLineRunner, vai permitir implementar 
+//um método auxiliar, pra executar alguma ação quando a aplicação iniciar
+//Para usar os métodos de salvar, lembrar sempre de criar a dependência CategoriaRepository
+//Depois de instanciar os objetos, você precisa salva-los no banco de dados 
+//usando o objeto categoriaRepository que foi declarado
+//
