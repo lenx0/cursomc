d7f18b440029f90ac0a2eefe2f1d32fbd0d9c50a
lenx0 <thiagofreitas1106@gmail.com>
2021-06-29 20:12:20 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/thiagodev/cursomc/CursomcApplication.java
+++ src/main/java/com/thiagodev/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.thiagodev.cursomc;
 
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.thiagodev.cursomc.domain.Categoria;
 import com.thiagodev.cursomc.domain.Cidade;
+import com.thiagodev.cursomc.domain.Cliente;
+import com.thiagodev.cursomc.domain.Endereco;
 import com.thiagodev.cursomc.domain.Estado;
 import com.thiagodev.cursomc.domain.Produto;
+import com.thiagodev.cursomc.domain.enums.TipoCliente;
 import com.thiagodev.cursomc.repositories.CategoriaRepository;
 import com.thiagodev.cursomc.repositories.CidadeRepository;
+import com.thiagodev.cursomc.repositories.ClienteRepository;
+import com.thiagodev.cursomc.repositories.EnderecoRepository;
 import com.thiagodev.cursomc.repositories.EstadoRepository;
 import com.thiagodev.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +33,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -72,10 +82,23 @@ public class CursomcApplication implements CommandLineRunner{
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-	}
-
+	
+	
+		Cliente cli1 = new Cliente(null, "Maria da Silva", "maria@gmail.com", "3213123123", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("30335544", "2221112"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "APTO 25", "Jardim", "3456789", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "652", "CASA", "Alvorada", "22313", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		
+	}		
 }
-
 
 //A implementação CommandLineRunner, vai permitir implementar 
 //um método auxiliar, pra executar alguma ação quando a aplicação iniciar
---
+++ src/main/java/com/thiagodev/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,40 @@
+package com.thiagodev.cursomc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"), // vai estar como 0 no banco de dados
+	PESSOAJURIDICA(2, "Pessoa Jurídica");// vai estar como 1 no banco de dados
+
+	private int cod;
+	private String descricao;
+
+	private TipoCliente(int cod, String descricao) { // Tipo enumerado o construtor é PRIVATE!!!
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static TipoCliente toEnum(Integer cod) {
+
+		if (cod == null) {
+			return null;
+		}
+
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+
+			}
+		}
+
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+
+}
+//CASO NÃO TIVER ENUMERADO OS NOMES, SE FOR ADICIONAR UM NOVO TIPO, ADICIONAR POR ÚLTIMO PARA NÃO ALTERAR A ORDEM!!!
